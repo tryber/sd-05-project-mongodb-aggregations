ea0c82852d9b7b5e322329d6b08dabd997a2c06a
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-11-04 14:47:15 -0300

    desafio 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,11 @@
+db.movies.aggregate(
+  [
+    {
+      $match: {"imdb.rating": {$gte: 7},
+      genres: {$not: {$in: ["Crime", "Horror"] } },
+      $or: [{rated: "PG"}, {rated: "G"} ],
+      languages: {$all: ["English", "Spanish"] }
+      }
+    }
+  ]
+).pretty();
