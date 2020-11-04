e4f61918c5c78dfe443a9fa6aea9e9ea327578d3
hleoc <informig@gmail.com>
2020-11-04 17:28:08 -0300

    Requisito 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,1 @@
+db.movies.aggregate([{$match: {$and: [{"imdb.rating": {$gte: 7}}, {$or: [{"genres": "Crime"}, {"genres": "Horror"}]}, {$or: [{"rated": "PG"}, {"rated": "G"}]}, {$and: [{"languages": "English"}, {"languages": "Spanish"}]}]}}]);
