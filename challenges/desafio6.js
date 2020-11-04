771a4557e1cb67b5f6f052421f9b2e8be1875cc4
Christian Duessel <duessel@gmail.com>
2020-11-04 19:10:34 -0300

    lint

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -6,27 +6,31 @@ db.movies.aggregate([
   },
   {
     $match: {
-      awards: { $regex: /oscar/i},
-      awards: { $regex: /won/i},
-    }
+      awards: { $regex: /oscar/i },
+    },
+  },
+  {
+    $match: {
+      awards: { $regex: /won/i },
+    },
   },
   {
     $group: {
       _id: null,
-      "maior_rating": { $max: "$imdb.rating" },
-      "menor_rating": { $min: "$imdb.rating" },
-      "Rmedia_rating": { $avg: "$imdb.rating" },
-      "Rdesvio_padrao": { $stdDevSamp: "$imdb.rating" },
-    }
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+    },
   },
   {
     $project:
     {
       _id: 0,
-      "maior_rating": 1,
-      "menor_rating": 1,
-      "media_rating": { $round: ["$Rmedia_rating", 1] },
-      "desvio_padrao": { $round: ["$Rdesvio_padrao", 1] }, 
-    }
-  }
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
+    },
+  },
 ]);
