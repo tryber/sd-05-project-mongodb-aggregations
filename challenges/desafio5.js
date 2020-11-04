2c0edc2757cc7afb123ba289cb14d7afd8600038
Felipe Vieira <flpvieira86@gmail.com>
2020-11-04 16:00:28 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,30 @@
+// Dands minha musa inspiradora
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+    },
+  },
+  {
+    $addFields: {
+      matchingActors: {
+        $setIntersection: [
+          [
+            "Sandra Bullock",
+            "Tom Hanks",
+            "Julia Roberts",
+            "Kevin Spacey",
+            "George Clooney",
+          ], "$cast",
+        ],
+      },
+    },
+  },
+  { $match: { matchingActors: { $ne: null } } },
+  { $addFields: { num_favs: { $size: "$matchingActors" } } },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  { $skip: 24 },
+  { $limit: 1 },
+  { $project: { title: 1, _id: 0 } },
+]);
