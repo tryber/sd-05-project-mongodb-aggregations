46505c56d11e8963a89bf17ade8b6256a25e4de2
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-11-04 15:04:46 -0300

    eslint nightmare [2]

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,10 +2,10 @@ db.movies.aggregate(
   [
     {
       $match: { "imdb.rating": { $gte: 7 },
-      genres: { $not: { $in: ["Crime", "Horror"] } },
-      $or: [{ rated: "PG" }, { rated: "G" } ],
-      languages: { $all: ["English", "Spanish"] }
-      }
-    }
-  ]
+        genres: { $not: { $in: ["Crime", "Horror"] } },
+        $or: [{ rated: "PG" }, { rated: "G" }],
+        languages: { $all: ["English", "Spanish"] }
+      },
+    },
+  ],
 ).pretty();
--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -2,10 +2,10 @@ db.movies.aggregate(
   [
     {
       $match: { "imdb.rating": { $gte: 7 },
-      genres: { $not: { $in: ["Crime", "Horror"] } },
-      $or: [{ rated: "PG" }, { rated: "G" } ],
-      languages: { $all: ["English", "Spanish"] }
-      }
+        genres: { $not: { $in: ["Crime", "Horror"] } },
+        $or: [{ rated: "PG" }, { rated: "G" }],
+        languages: { $all: ["English", "Spanish"] },
+      },
     },
     {
       $project:
@@ -15,8 +15,8 @@ db.movies.aggregate(
           notaIMDB: "$imdb.rating",
           votosIMDB: "$imdb.votes",
           ano: "$year",
-          _id: 0
-        }
-    }
-  ]
+          _id: 0,
+        },
+    },
+  ],
 ).pretty();
