20494c24a56aa2c96a15cf7b9e3a66a1474e237d
hleoc <informig@gmail.com>
2020-11-04 18:15:03 -0300

    Requisito 1 - corrigindo erros parte 3

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -3,8 +3,8 @@ db.movies.aggregate([
     $match: {
       $and: [
         { "imdb.rating": { $gte: 7 } },
-        { genres: {$nin: ["Crime", "Horror"]} },
-        { rated: {$in: ["PG", "G"]} },
+        { genres: { $nin: ["Crime", "Horror"] } },
+        { rated: { $in: ["PG", "G"] } },
         { $and: [{ languages: "English" }, { languages: "Spanish" }] },
       ],
     },
