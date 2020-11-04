795d5e45b6a4021a20f65d6c4c3d6e6e2048e331
hleoc <informig@gmail.com>
2020-11-04 17:33:14 -0300

    Requisito 1 - corrigindo erros do Eslint

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,12 @@
-db.movies.aggregate([{$match: {$and: [{"imdb.rating": {$gte: 7}}, {$or: [{"genres": "Crime"}, {"genres": "Horror"}]}, {$or: [{"rated": "PG"}, {"rated": "G"}]}, {$and: [{"languages": "English"}, {"languages": "Spanish"}]}]}}]);
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { "imdb.rating": { $gte: 7 } },
+        { $or: [{ genres: "Crime" }, { genres: "Horror" }] },
+        { $or: [{ rated: "PG" }, { rated: "G" }] },
+        { $and: [{ languages: "English" }, { languages: "Spanish" }] },
+      ],
+    },
+  },
+]);
