9987209feee927ce52224f916df147f98a9fcb78
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-11-05 16:47:37 -0300

    Mudanças no desafio 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -6,3 +6,27 @@
 // de palavras presentes em title e retorne apenas o novo campo title_split dos filmes com o título
 // composto apenas de uma palavra, ordernando-os por title em ordem alfabética.
 // Por exemplo, "Cinderela" e "3-25" devem entrar nessa contagem, mas "Cast Away" não.
+
+db.movies.aggregate([
+  {
+    $addFields: {
+      title_size: { $size: { $split: ["$title", " "] } },
+    },
+  },
+  {
+    $match: {
+      title_size: 1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      title_split: ["$title"],
+    },
+  },
+  {
+    $sort: {
+      title_split: 1,
+    },
+  },
+]);
