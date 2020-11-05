afde540fb5bc6efd2adbf071c56d7d63ca495fe7
Christian Duessel <duessel@gmail.com>
2020-11-05 16:47:02 -0300

    req 13

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,10 +1,30 @@
 db.trips.aggregate([
   {
     $match: {
-      startTime: { $eq: Date("2016-03-10") },
+      startTime: {
+        $gte: ISODate("2016-03-10T00:00:00Z"),
+        $lte: ISODate("2016-03-10T23:59:59Z"),
+      },
     },
   },
-]).pretty();
+  {
+    $addFields: {
+      duracao: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000 * 60] },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMedia: { $avg: "$duracao" },
+    },
+  },
+  {
+    $project: {
+      duracaoMediaEmMinutos: { $ceil: "$duracaoMedia" },
+      _id: 0,
+    },
+  },
+]);
 
 // 10/03/2016
 // { "duracaoMediaEmMinutos" : <duracao_media_em_minutos> }
