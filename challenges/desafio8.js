f3ee365ca2285467e779e35ae2f726dd27e1bfab
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-11-05 18:50:55 -0300

    solucao alternativa ao desafio 8 adicionada

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,3 +1,4 @@
+//  levou 1.9s :
 db.air_alliances.aggregate(
   [
     { $unwind: "$airlines" },
@@ -11,3 +12,38 @@ db.air_alliances.aggregate(
     { $limit: 1 },
   ],
 );
+
+//  outra resolucao com menor perfomance, levou 2.6 s:
+// db.air_alliances.aggregate(
+//   [
+//     { $unwind: "$airlines" },
+//     { $lookup:
+//       {
+//         from: "air_routes",
+//         let: { referenciaNomeCia: "$airlines" }, // ref ao air_alliances
+//         pipeline:
+//         [
+//           { $match:
+//             { $expr:
+//               {
+//                 $and:
+//                   [
+//                     {$eq: ["$$referenciaNomeCia", "$airline.name"] },
+//                     {$or: [{$eq: ["$airplane", "747" ] }, {$eq: ["$airplane", "380" ] }] },
+//                   ]
+//               }
+//             }
+//           }
+//         ],
+//         as: "rotas",
+//       }
+//     },
+//     { $unwind: "$rotas" },
+//     { $match: { "rotas.airplane": { $in: ["747", "380"] } } },
+//     { $group: { _id: "$name", totalRotas: { $sum: 1 } } },
+//     { $sort: { totalRotas: -1 } },
+//     { $limit: 1 },
+//   ],
+// );
+
+//  https://docs.mongodb.com/manual/reference/operator/aggregation/lookup/
