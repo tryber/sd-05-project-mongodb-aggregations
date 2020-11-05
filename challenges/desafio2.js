501112443c8a1b863a692c1b9b0d791441d9b9e3
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-11-05 20:11:34 -0300

    sem problemas com codeclimate agora

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -3,8 +3,8 @@ db.movies.aggregate(
     {
       $match: { "imdb.rating": { $gte: 7 },
         genres: { $nin: ["Crime", "Horror"] },
-        $or: [{ rated: "PG" }, { rated: "G" }],
         languages: { $all: ["English", "Spanish"] },
+        $or: [{ rated: "PG" }, { rated: "G" }],
       },
     },
     {
