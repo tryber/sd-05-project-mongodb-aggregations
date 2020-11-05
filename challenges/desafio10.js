01bd9ae00ae63662264eba48afcecf81250d0eac
Christian Duessel <duessel@gmail.com>
2020-11-05 15:34:13 -0300

    req10

---
+++ challenges/desafio10.js
@@ -0,0 +1,25 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      duracao: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000*60] }
+    },
+  },
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: { $avg: "$duracao" },
+    },
+  },
+  {
+    $project: {
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$duracaoMedia", 2] },
+      _id: 0,
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: -1,
+    },
+  },
+]);
