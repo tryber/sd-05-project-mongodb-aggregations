1551bda552e447ecc0514684fb5e69f875958fd1
Christian Duessel <duessel@gmail.com>
2020-11-05 15:08:42 -0300

    req9

---
+++ challenges/desafio9.js
@@ -0,0 +1,24 @@
+db.trips.aggregate([
+  {
+    $match: { birthYear: { $ne: "" } },
+  },
+  {
+    $addFields: {
+      numberYear: {$toInt: "$birthYear"},
+    },
+  },
+  {
+    $group: { 
+      _id: null,
+      maiorAnoNascimento: { $max: "$numberYear" },
+      menorAnoNascimento: { $min: "$numberYear" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: 1,
+      menorAnoNascimento: 1,
+    },
+  },
+]);
