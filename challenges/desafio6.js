cc98532e98683c069dc09d6a58ac001f6f339a62
Preto115 <preto115@gmail.com>
2020-11-08 00:53:53 -0300

    desafio_06.js

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -23,28 +23,34 @@ O resultado da sua query deve ter o seguinte formato:
 }
 
 */
+// [HONESTIDADE ACADEMICA] - Aqui foi pesquisado uma maneira de validar o regex
+// para procurar por expressoes específicas, tive o suporte necessario atraves de
+// projetos no gitHub  PR consultado => Rodrigo Batista
 
 db.movies.aggregate([
   {
     $match:
     {
-      awards: { $exists: 1 },
+      awards: { $regex: /won [0-9] oscar/i,
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
     },
   },
   {
     $project: {
-      // _id: null,
-      maior_rating: { $max: "$imdb.rating" },
-      // menor_rating: { $min: { $sum: "$imdb.rating" } },
-      // media_rating: { $avg: { $sum: "$imdb.rating" } },
-
+      _id: 0,
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
     },
   },
-  // {
-  //   $project: {
-  //     maior_rating: 1,
-  //     menor_rating: 1,
-  //     media_rating: 1,
-  //   },
-  // },
 ]).pretty();
