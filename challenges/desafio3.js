9c8ff5e93adf5cb03ff49c3ef6250b4c5427a102
Preto115 <preto115@gmail.com>
2020-11-07 23:09:44 -0300

    refatoring desafio-3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -42,7 +42,10 @@ db.movies.aggregate([
     $sort: {
       ano: -1,
       notaIMDB: -1,
-      titulo: 1,
+    },
+  },
+  {
+    $sort: { titulo: 1,
     },
   },
 ]).pretty();
--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -23,3 +23,28 @@ O resultado da sua query deve ter o seguinte formato:
 }
 
 */
+
+db.movies.aggregate([
+  {
+    $match:
+    {
+      awards: { $exists: 1 },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      // menor_rating: { $min: "$imdb.rating" },
+      // media_rating: { $avg: "$imdb.rating" },
+
+    },
+  },
+  // {
+  //   $project: {
+  //     maior_rating: 1,
+  //     menor_rating: 1,
+  //     media_rating: 1,
+  //   },
+  // },
+]).pretty();
