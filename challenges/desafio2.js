45f3f95209f76aa9ec70f68f2f92f186cb330b3a
pauloricardoz <trybe.przt@gmail.com>
2020-11-09 16:49:00 -0300

    desafio 2 using $ in the data

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -13,10 +13,10 @@ db.movies.aggregate([
     $project: {
       _id: 0,
       titulo: "$title",
-      avaliado: "rated",
-      notaIMDB: "imdb.rating",
-      votosIMDB: "imdb.votes",
-      ano: "year",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
     },
   },
 ]);
