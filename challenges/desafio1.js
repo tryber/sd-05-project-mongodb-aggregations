e5f09b53812513fe6e5f9213baa2e396931d7494
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-11-09 19:12:13 -0300

    Desafio 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,12 @@
+db.movies.aggregate(
+  [
+    {
+      $match: {
+        genres: { $nin: ["Crime", "Horror"] },
+        rated: { $in: ["PG", "G"] },
+        "imdb.rating": { $gte: 7 },
+        languages: { $all: ["English", "Spanish"] },
+      },
+    }
+  ]
+);
