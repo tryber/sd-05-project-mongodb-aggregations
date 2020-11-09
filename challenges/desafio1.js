31ae53fc5f8f42ea4a73c4e0754be4b680054ed3
saturnian99 <saturnian.dsg@gmail.com>
2020-11-09 17:16:30 -0300

    Challenge #1

---
+++ challenges/desafio1.js
@@ -0,0 +1,12 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"]}
+    }
+  },
+  { $project: { _id: 0, "imdb.rating": 1, genres: 1, rated: 1, languages: 1 } },
+  { $count: "documents" }
+]);
