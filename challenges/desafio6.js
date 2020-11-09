4654476843db805473cfeb549104a4989c7bdd1d
Mau <mauricio.sdjr@gmail.com>
2020-11-09 17:41:30 -0300

    desafio 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,26 @@
+// regex retirado do repo de colegas
+db.movies.aggregate([
+  {
+    $match: {
+      awards: { $regex: /Won (?:1(?:[0-9] Oscars| Oscar)|[2-9] Oscars)/ },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
+      _id: 0,
+    },
+  },
+]);
