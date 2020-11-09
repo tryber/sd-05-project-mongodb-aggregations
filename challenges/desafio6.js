997fe814f2f2358f18162811d5c3bcca20e997bd
Luis Medeiros <luis.codemaker@gmail.com>
2020-11-08 21:55:34 -0300

    ex06

---
+++ challenges/desafio6.js
@@ -0,0 +1,28 @@
+db.movies.aggregate([
+  {
+    $match: {
+      awards: { $regex: /Won \d Oscars/gi },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      maior_rating: true,
+      menor_rating: true,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
+      _id: false,
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
