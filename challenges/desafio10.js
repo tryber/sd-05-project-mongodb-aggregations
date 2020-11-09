82682002c8b5bda005543c89bff2a332ccac514a
Virginia Duca <virginiambd.bio@gmail.com>
2020-11-09 18:36:49 -0300

    cc

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,26 +2,26 @@ db.trips.aggregate([
   {
     $addFields: {
       duracao: {
-        $subtract: ["$stopTime", "$startTime"]
-      }
-    }
+        $subtract: ["$stopTime", "$startTime"],
+      },
+    },
   },
   {
     $group: {
       _id: "$usertype",
-      duracaoMedia: { $avg: "$duracao"}
-    }
+      duracaoMedia: { $avg: "$duracao" },
+    },
   },
   {
     $sort: {
-      duracaoMedia: 1
-    }
+      duracaoMedia: 1,
+    },
   },
   {
     $project: {
       _id: 0,
       tipo: "$_id",
-      duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", 3600000]}, 2]}
-    }
+      duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", 3600000] }, 2] },
+    },
   },
 ]);
