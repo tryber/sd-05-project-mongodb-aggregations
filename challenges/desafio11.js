3f903bf48b91893c9ad8ee225cb3e5ef01ff221f
ferrari <luisdiasferrari@gmail.com>
2020-11-09 15:10:31 -0300

    Desafio 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -5,3 +5,20 @@
 //
 // O resultado da sua query deve ter o seguinte formato:
 // { "diaDaSemana" : <dia_da_semana>, "total" : <total_de_viagens> }
+
+db.trips.aggregate([
+  { $addFields: { 
+    diaDaSemana: { $dayOfWeek: "$startTime" },
+  } },
+  { $group: {
+    _id: "$diaDaSemana",
+    total: { $sum: 1 },
+  }},
+  { $project: {
+    _id: 0, diaDaSemana: "$_id", total: "$total",
+  } },
+  { $sort: {
+    total: -1
+  } },
+  { $limit: 1 },
+]).pretty();
