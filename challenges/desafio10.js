14d291ad6dc8335fefa0a4d3d9a23e5049e17ed1
pauloricardoz <trybe.przt@gmail.com>
2020-11-10 02:18:41 -0300

    desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,11 @@
+db.trips.aggregate([
+  { $match: {
+    usertype: { $exists: true } },
+  },
+  { $group: {
+    _id: "$usertype",
+    duracaoMedia: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+  } },
+  { $project: { _id: 1, duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", 3600000] }, 2] } } },
+  { $sort: { _id: -1 } },
+]);
