808ebf7bad0a64dd7789d8a0b687a6c5e989a7bc
ferrari <luisdiasferrari@gmail.com>
2020-11-09 23:32:34 -0300

    Desafio 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -9,3 +9,16 @@
 // { "bikeId" : <bike_id>, "duracaoMedia" : <duracao_media> }
 // { "bikeId" : <bike_id>, "duracaoMedia" : <duracao_media> }
 // { "bikeId" : <bike_id>, "duracaoMedia" : <duracao_media> }
+
+db.trips.aggregate([
+  { $addFields: { tempoVoo: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60 * 1000] } } },
+  { $group: {
+    _id: "$bikeid",
+    duracaoMedia: { $avg: "$tempoVoo" },
+  } },
+  { $project: {
+    _id: 0, bikeId: "$_id", duracaoMedia: { $ceil: "$duracaoMedia" },
+  } },
+  { $sort: { duracaoMedia: -1 } },
+  { $limit: 5 },
+]).pretty();
