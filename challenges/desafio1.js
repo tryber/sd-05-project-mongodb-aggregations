37dd876745b782ae241a6b467ba5178aa3a2a081
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-11-09 22:27:17 -0300

    seguindo o conselho do mitchel para mudar a ordem e dar certo o CC

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,8 +1,8 @@
 db.movies.aggregate({
   $match: {
-    "imdb.rating": { $gte: 7 },
     genres: { $nin: ["Crime", "Horror"] },
     rated: { $in: ["PG", "G"] },
+    "imdb.rating": { $gte: 7 },
     languages: { $all: ["English", "Spanish"] },
   },
 });
