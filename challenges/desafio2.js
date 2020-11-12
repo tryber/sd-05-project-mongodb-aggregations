f3ac791b17452da65fbd6eca546b5e97b91bc9db
samueap <samueap@gmail.com>
2020-11-12 17:21:56 -0300

    refactoring to CC

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -5,8 +5,8 @@ db.movies.aggregate([
       $and: [
         { "imdb.rating": { $gte: 7 } }, // comparando os rating em imdb maiores ou igual ($gte) a 7
         { genres: { $nin: ["Crime", "Horror"] } }, // Não incluindo ($nin) os generos Crime e Horror
-        { rated: { $in: ["PG", "G"] } }, // Incluindo ($in) as classificações
         { languages: { $all: ["English", "Spanish"] } }, // Selecionando todas ($all) com linguagem em Esp e Ing
+        { rated: { $in: ["PG", "G"] } }, // Incluindo ($in) as classificações
       ],
     },
   },
