dcb31b03d813d4f53df7dcfde748a5baa4282128
samueap <samueap@gmail.com>
2020-11-12 10:55:50 -0300

    Req 1 and 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,22 @@
+db.movies.aggregate([ // Seleciono(aggregates) os movies
+  {
+    $match: {
+      $and: [
+        { "imdb.rating": { $gte: 7 } }, // comparando os rating em imdb maiores ou igual ($gte) a 7
+        { genres: { $nin: ["Crime", "Horror"] } }, // Não incluindo ($nin) os generos Crime e Horror
+        { rated: { $in: ["PG", "G"] } }, // Incluindo ($in) as classificações
+        { languages: { $all: ["English", "Spanish"] } }, // Selecionando todas ($all) com linguagem em Esp e Ing
+      ],
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title", // Modifica $title para titulo
+      avaliado: "$rated", // Modifica $rated para avaliado
+      notaIMDB: "$imdb.rating", // Modifica $imdb.rating para notaIMDB
+      votosIMDB: "$imdb.votes", // Modifica $imdb.votes para votosIMDB
+      ano: "$year", // Modifica $year para ano
+    },
+  },
+]);
