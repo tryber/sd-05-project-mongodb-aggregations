4fea802475722bd51b36956d92b57b44258efeb9
Eder Sena <eder.sena@live.com>
2020-11-16 15:47:53 -0300

    Projecto Aggregationtsu

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,20 +1,20 @@
 db.movies.aggregate([
   {
     $match: {
+      "imdb.rating": { $gte: 7 },
       languages: { $all: ["English", "Spanish"] },
       rated: { $in: ["PG", "G"] },
-      "imdb.rating": { $gte: 7 },
       genres: { $nin: ["Crime", "Horror"] },
     },
   },
   {
     $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
       notaIMDB: "$imdb.rating",
       votosIMDB: "$imdb.votes",
       ano: "$year",
-      avaliado: "$rated",
-      titulo: "$title",
-      _id: 0,
     },
   },
 ]);
