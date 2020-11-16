82aadf3a7a9df8b79e3e731f72187b93ce63a57f
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-11-16 15:05:28 -0300

    invertendo as ordens de blocos identicos

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,10 +1,10 @@
 db.movies.aggregate([
   {
     $match: {
-      "imdb.rating": { $gte: 7 },
-      rated: { $in: ["PG", "G"] },
-      languages: { $all: ["English", "Spanish"] },
       genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["G", "PG"] },
+      "imdb.rating": { $gte: 7 },
+      languages: { $all: ["English", "Spanish"] },
     },
   },
   {
