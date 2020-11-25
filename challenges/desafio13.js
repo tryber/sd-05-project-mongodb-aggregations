4e410381712eb6763b94aa3ad44e784d971f06e1
saturnian99 <saturnian.dsg@gmail.com>
2020-11-25 19:14:09 -0300

    Removed all challenge's descriptions

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,12 +1,3 @@
-/*
-Desafio 13
-Determine a duração média das viagens iniciadas no dia 10/03/2016, em minutos. Arredonde o resultado para cima.
-
-O resultado da sua query deve ter o seguinte formato:
-
-{ "duracaoMediaEmMinutos" : <duracao_media_em_minutos> }
-*/
-
 db.trips.aggregate([
   {
     $addFields: {
