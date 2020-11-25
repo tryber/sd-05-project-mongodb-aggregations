d2a910b812814d96b288c9912e360161f1c9f72e
saturnian99 <saturnian.dsg@gmail.com>
2020-11-25 19:06:45 -0300

    Added all challenge's descriptions

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,3 +1,13 @@
+/*
+Desafio 10
+Encontre a média de viagens por tipo de usuário. Exiba o valor em horas com apenas duas casas decimais e a média de viagens ordenada de forma crescente. Para arredondar a média use o $round.
+
+O resultado da sua query deve ter o seguinte formato:
+
+{ "tipo" : <tipo>, "duracaoMedia" : <duracaoMedia> }
+// ...
+*/
+
 db.trips.aggregate([
   {
     $addFields: {
@@ -5,7 +15,7 @@ db.trips.aggregate([
     }
   },
   {
-   $group: {
+    $group: {
       _id: "$usertype",
       duracaoMedia: { $avg: "$duracao" }
     }
