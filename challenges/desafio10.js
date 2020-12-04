dc06c4db7458bf437e4ed8692d4615f416c4838d
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-12-03 23:10:36 -0300

    Desafio 10 CHECAR! NAO TA PASSANDO

---
+++ challenges/desafio10.js
@@ -0,0 +1,32 @@
+const millisec = 1000;
+const sec = 60;
+const min = 60;
+
+db.trips.aggregate(
+  [
+    {
+      $group: {
+        _id: "$userType",
+        duracaoMedia: {
+          $avg: {
+            $divide: [{
+              $subtract: ["$stopTime", "$startTime"],
+            },
+            millisec * sec * min
+            ],
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        tipo: "$_id",
+        duracaoMedia: { $round: ["$duracaoMedia", 2] },
+      },
+    },
+    {
+      $sort: { duracaoMedia: 1 },
+    },
+  ]
+);
