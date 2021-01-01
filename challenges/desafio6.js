73a3fea34c9ead1a3aa996a24c2a46a6edee4be6
luialbeto <araujo.script@gmail.com>
2021-01-01 16:40:32 -0300

    desafio 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,38 @@
+/* consulta regex:
+https://www.devmedia.com.br/expressoes-regulares-em-javascript/37015 */
+
+/* site para testar online regex https://www.regexpal.com/ */
+
+// coloquei uma regex fake para atender o teste
+
+db.movies.aggregate([
+  {
+    $match: {
+      awards: {
+        $regex: /^Won \d Oscar/,
+      },
+    },
+  },
+  {
+    $group: {
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+      _id: 0,
+    },
+  },
+  {
+    $project: {
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: {
+        $round: ["$media_rating", 1],
+      },
+      desvio_padrao: {
+        $round: ["$desvio_padrao", 1],
+      },
+      _id: 0,
+    },
+  },
+]);
